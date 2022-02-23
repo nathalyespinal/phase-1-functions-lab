1a9ef7e33da70d95941da32d833f7c9541bf5267
NathalyEspinal <espinalnathaly@gmail.com>
2022-02-23 16:25:48 -0600

    commit tree 513150c8c084ae5d70d3409fd0715d4fd559f1d3

---
+++ Practice.js
@@ -0,0 +1,29 @@
+function isbiggerthan400(start, destination) {
+    // 600 fare is =2 cents/foot  600*2
+    //1500 2ct/fooot  1500*2 
+    //generalize
+    if (distanceInFeet <= 400) {
+        return 
+        //** is between <401&2000)
+    //think how tell if  anumber is begger than 400 and smaller than 2000
+    
+    Math.max()
+    return 2 * 
+    
+    distanceInFeet 2001 and 2500
+    }
+
+
+
+
+//generalize
+if (distanceInFeet start <= 400) {
+    return 
+    //** is between <401&2000)
+//think how tell if  anumber is begger than 400 and smaller than 2000
+
+Math.max()
+return 2 * 
+
+distanceInFeet 2001 and 2500
+}
--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-// Code your solution in this file!
+const hq = 42
+function distanceFromHqInBlocks(pickuplocation){
+    if (pickuplocation <= hq){
+    return hq - pickuplocation;
+    } else if (pickuplocation > hq ) {
+    return pickuplocation -hq;
+}
+}
+
+
+function distanceFromHqInFeet(locationInBlocks) {
+return distanceFromHqInBlocks(locationInBlocks) * 264
+}
+
+
+
+function distanceTravelledInFeet (start, destination) {
+return Math.abs(destination - start) * 264 
+}
+
+
+function calculatesFarePrice(start, destination) {
+
+// four potential return values, based on distance travelled in feet
+// calculate the distance traveled in feet
+
+let feetTravelled = distanceTravelledInFeet(start, destination)
+
+if(feetTravelled<=400){
+    return 0
+}else if (feetTravelled>=400 && feetTravelled<=2000){
+    return (feetTravelled - 400 ) * 0.02
+}else if (feetTravelled >=2000 && feetTravelled<2500){
+    return 25
+}else { 
+  return 'cannot travel that far'
+}
+
+
+
+// const fare = distanceTravelledInFeet - 400 * .02
+
+
+// if (distace >= 2500)
+// return 'cannot travel that far'
+}
+
+// calculatesFarePrice: Given the same starting and ending block as the previous test (hint hint), return the fare for the customer. 
+// The first four hundred feet are free. For a distance between 400 and 2000 feet, the price is 2 cents per foot (not including 400, which are free!). 
+// Then Scuber charges a flat fare for a distance over 2000 feet and under 2500 feet. 
+//Finally, Scuber does not allow any rides over 2500 feet — the function returns 'cannot travel that far' if a ride over 2500 feet is requested.
+
+
+// //1320 feet 
+
+
+
